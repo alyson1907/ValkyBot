1a6015f3b7c25b2ea8f1ac61741a8b8f1bbe0b04
Alyson Maruyama <alyson1907@gmail.com>
2020-12-11 19:32:48 -0300

    Fixed commandName and command args parsing @music.js handler

--- commands/music.js
+++ commands/music.js
@@ -4,13 +4,13 @@ const state = {
   queue: [],
 }
 
-const playAudio = async (message, command) => {
+const playAudio = async (message, commandArgs) => {
   if (message.channel.type !== 'text') return
 
   const voiceChannel = message.member.voice.channel
   if (!voiceChannel) return message.reply('You need to be in a Voice Channel!')
 
-  const videoURL = 'https://www.youtube.com/watch?v=tPEE9ZwTmy0' // command.args[0]
+  const videoURL = 'https://www.youtube.com/watch?v=tPEE9ZwTmy0' // command.args.shift()
   state.queue.push(videoURL)
 
   const stream = ytdl(videoURL, { filter: 'audioonly', volume: 0.02, bitrate: 128 })
@@ -28,10 +28,12 @@ const leaveVoiceChannel = (message) => {
 }
 
 const musicHandler = (message, command) => {
-  const commandName = command.args.shift()
+  const commandName = command.command
+  const commandArgs = command.args
+
   switch (commandName) {
   case 'play':
-    return playAudio(message, command)
+    return playAudio(message, commandArgs)
 
   case 'leave':
     return leaveVoiceChannel(message)
